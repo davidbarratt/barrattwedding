b7990735533ae41e8d20fb4fe53c161e0baf7863
David Barratt <david@davidwbarratt.com>
2013-10-01 19:44:13 -0400

    Generate the SocialBundle

--- app/AppKernel.php
+++ app/AppKernel.php
@@ -19,6 +19,7 @@ class AppKernel extends Kernel
             new Doctrine\Bundle\MigrationsBundle\DoctrineMigrationsBundle(),
             new Wedding\RespondBundle\WeddingRespondBundle(),
             new Wedding\TwitterBundle\WeddingTwitterBundle(),
+            new Wedding\SocialBundle\WeddingSocialBundle(),
         );
 
         if (in_array($this->getEnvironment(), array('dev', 'test'))) {
---
+++ src/Wedding/SocialBundle/WeddingSocialBundle.php
@@ -0,0 +1,9 @@
+<?php
+
+namespace Wedding\SocialBundle;
+
+use Symfony\Component\HttpKernel\Bundle\Bundle;
+
+class WeddingSocialBundle extends Bundle
+{
+}

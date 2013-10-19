cf4b3d460d9b2ef98b578c1cc1aaebf38f8880c6
David Barratt <david@davidwbarratt.com>
2013-10-19 17:17:50 -0400

    Revert "Generate the SocialBundle"
    This reverts commit b7990735533ae41e8d20fb4fe53c161e0baf7863.

--- app/AppKernel.php
+++ app/AppKernel.php
@@ -19,7 +19,6 @@ class AppKernel extends Kernel
             new Doctrine\Bundle\MigrationsBundle\DoctrineMigrationsBundle(),
             new Wedding\RespondBundle\WeddingRespondBundle(),
             new Wedding\TwitterBundle\WeddingTwitterBundle(),
-            new Wedding\SocialBundle\WeddingSocialBundle(),
         );
 
         if (in_array($this->getEnvironment(), array('dev', 'test'))) {
--- src/Wedding/SocialBundle/WeddingSocialBundle.php
+++
@@ -1,9 +0,0 @@
-<?php
-
-namespace Wedding\SocialBundle;
-
-use Symfony\Component\HttpKernel\Bundle\Bundle;
-
-class WeddingSocialBundle extends Bundle
-{
-}
